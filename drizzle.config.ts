fa0bc0c682a6f24bb90d90808b4d43fde3b301ba
sidik nuryahya <cahyasidiknur@gmail.com>
2026-07-03 01:27:38 +0700

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  dialect: 'postgresql',
-  schema: './db/schema.ts',
-  out: 'netlify/database/migrations',
-})
+    schema: "./db/schema/*",
+    out: "./db/migrations",
+    dialect: "postgresql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL!
+    }
+});
